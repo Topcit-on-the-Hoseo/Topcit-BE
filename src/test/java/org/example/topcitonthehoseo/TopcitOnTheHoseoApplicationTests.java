f7b4bc27c1f7ca9614d102a99f3ff52dd2cf6eb4
syuh23 <seonghee5070@gmail.com>
2025-05-27 16:09:57 +0900

    start setting

---
+++ src/test/java/org/example/topcitonthehoseo/TopcitOnTheHoseoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.topcitonthehoseo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopcitOnTheHoseoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
