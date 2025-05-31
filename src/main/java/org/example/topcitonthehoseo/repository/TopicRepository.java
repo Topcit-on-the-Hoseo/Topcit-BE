1e82446de82e51644302833d2cbefd68e845d413
syuh23 <seonghee5070@gmail.com>
2025-05-31 17:37:15 +0900

    save questions in question dir to databases

--- src/main/java/org/example/topcitonthehoseo/repository/TopicRepository.java
+++ src/main/java/org/example/topcitonthehoseo/repository/TopicRepository.java
@@ -4,8 +4,11 @@ import org.example.topcitonthehoseo.entity.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TopicRepository extends JpaRepository<Topic, Long> {
 
     List<Topic> findByLecture_LectureId(Integer lectureId);
+
+    Optional<Topic> findByTopicName(String topicName);
 }
